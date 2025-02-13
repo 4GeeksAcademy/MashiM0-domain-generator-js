9fabe5866ef0e8f9660cb50bffa833e22b12de2d
MashiM0 <massimog.boschettib@gmail.com>
2025-02-13 00:01:34 +0000

    [change loops key words]

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
  //write your code here
-  let pronoun = ['the', 'our'];
-  let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon'];
-  let domain = ['.com','.us','.ve','.tv']
+  let pronouns = ['the', 'our'];
+  let adjs = ['great', 'big'];
+  let nouns = ['jogger', 'racoon'];
+  let domains = ['.com','.us','.ve','.tv']
 
-    for(let i=0;i < pronoun.length;i++){
-      for(let j=0; j < adj.length; j++){
-        for(let k=0; k < noun.length; k++){
-          for(let l=0; l< domain.length; l++){
-            console.log(pronoun[i] + adj[j]+ noun[k] + domain[l])
+    for(const pronoun of pronouns){
+      for(const adj of adjs){
+        for(const noun of nouns){
+          for(const domain of domains){
+            console.log(pronoun+adj+noun+domain)
           }
         }
       }
     }
+
+    // Another way
+        // for(let i=0;i < pronoun.length;i++){
+    //   for(let j=0; j < adj.length; j++){
+    //     for(let k=0; k < noun.length; k++){
+    //       for(let l=0; l< domain.length; l++){
+    //         console.log(pronoun[i] + adj[j]+ noun[k] + domain[l])
+    //       }
+    //     }
+    //   }
+    // }
